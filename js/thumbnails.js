a1f4740b23321f8ef7629221fe56dfd0a4cceb0d
ElenaSalak <len.salack@yandex.ru>
2024-10-04 23:00:58 +0700

    Правки 04.10

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -1,6 +1,6 @@
 import { postsData } from './createArray.js';
 
-const template = document.querySelector('#picture').textContent.querySelector('.picture');
+const template = document.querySelector('#picture').content.querySelector('.picture');
 const container = document.querySelector('.pictures');
 
 const createThumbnail = (photo) => {
@@ -8,7 +8,7 @@ const createThumbnail = (photo) => {
   const image = thumbnail.querySelector('.picture__img');
 
   image.src = photo.url;
-  image.alt = photo.deccriprion;
+  image.alt = photo.decsriprion;
 
   thumbnail.querySelector('.picture__comments').textContent = photo.comments.length;
   thumbnail.querySelector('.picture__likes').textContent = photo.likes;
