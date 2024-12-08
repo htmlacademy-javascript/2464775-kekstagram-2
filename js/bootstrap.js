1936c7f3b23c77dd0b7231b63a431031f74b205d
ElenaSalak <len.salack@yandex.ru>
2024-12-08 19:33:59 +0700

    11

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,4 +1,5 @@
-import { openBigPicture } from './render-photo.js';
+// import { openBigPicture } from './render-photo.js';
+import { describePhoto } from './createArray.js';
 import { getData } from './api.js';
 import { createThumbnail } from './thumbnails.js';
 import { showErrorMessage } from './utils.js';
@@ -6,7 +7,7 @@ import { showErrorMessage } from './utils.js';
 const bootstrap = async () => {
   try {
     const photo = await getData();
-    openBigPicture(photo);
+    describePhoto(photo);
     createThumbnail(photo);
   } catch (error) {
     showErrorMessage(error.message);
