db7757aab00ef12664800e47da3f2a75100964f4
ElenaSalak <len.salack@yandex.ru>
2024-12-24 11:48:48 +0700

    Правки в модуле notification.js

--- js/notification.js
+++ js/notification.js
@@ -1,30 +1,23 @@
 const body = document.body;
 
+const isEscPressed = (event) => event.key === 'Escape' || event.key === 'Esc';
+
 const closeNotification = (evt) => {
   evt.stopPropagation();
   const existElement = document.querySelector('.success') || document.querySelector('.error');
-  const closeButton = existElement.querySelector('button');
 
-  const onEscKeydown = (event) => {
-    if (event.key === 'Escape' || event.key === 'Esc') {
-      existElement.remove();
-      body.removeEventListener('keydown', onEscKeydown);
-    }
-  };
-
-  if (evt.target === existElement || evt.target === closeButton) {
+  if (isEscPressed(evt) || evt.target.closest('button') || !evt.target.closest('.success') || !evt.target.closest('.error')) {
     existElement.remove();
     body.removeEventListener('click', closeNotification);
-    body.removeEventListener('keydown', onEscKeydown);
+    body.removeEventListener('keydown', isEscPressed);
   }
-
-  body.addEventListener('keydown', onEscKeydown);
 };
 
 const appendNotification = (template) => {
   const notificationNode = template.cloneNode(true);
   body.append(notificationNode);
   body.addEventListener('click', closeNotification);
+  body.addEventListener('keydown', isEscPressed);
 };
 
 export { appendNotification };
